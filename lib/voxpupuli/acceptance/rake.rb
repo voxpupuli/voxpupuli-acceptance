a7fbf612133c855dee879f38ffce9285c3ef1442
Tim Meusel <tim@bastelfreak.de>
2022-01-21 14:15:00 +0100

    always load tasks from beaker-rspec

--- lib/voxpupuli/acceptance/rake.rb
+++ lib/voxpupuli/acceptance/rake.rb
@@ -1,6 +1,9 @@
 begin
   # Fixtures can be needed because of spec_prep
+  # spec_prep taks is provided by puppetlabs_spec_helper
   require 'puppetlabs_spec_helper/tasks/fixtures'
 rescue LoadError
-  require 'beaker-rspec/rake_task'
+  # we only need that during CI, so we rescue the LoadError
 end
+# provides the beaker rake task
+require 'beaker-rspec/rake_task'
