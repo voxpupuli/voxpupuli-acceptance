voxpupuli/voxpupuli-acceptance

commit d89ef932b74a2327fb5201699dd0d62c7e2734af

Implement OpenVox support
Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

lib/voxpupuli/acceptance/spec_helper_acceptance.rb
@@ -21,9 +21,9 @@ def configure_beaker(modules: :metadata, &block)
       unless %w[none preinstalled].include?(collection)
         BeakerPuppetHelpers::InstallUtils.install_puppet_release_repo_on(host, collection)
       end
-      package_name = ENV.fetch('BEAKER_PUPPET_PACKAGE_NAME',
-                               BeakerPuppetHelpers::InstallUtils.puppet_package_name(host,
-                                                                                     prefer_aio: collection != 'none'))
+      package_name = ENV.fetch('BEAKER_PUPPET_PACKAGE_NAME') do
+        BeakerPuppetHelpers::InstallUtils.collection2packagename(host, collection, prefer_aio: collection != 'none')
+      end
       host.install_package(package_name)
 
       # by default, puppet-agent creates /etc/profile.d/puppet-agent.sh which adds /opt/puppetlabs/bin to PATH

voxpupuli-acceptance.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'beaker-docker', '~> 2.1'
   s.add_dependency 'beaker-hiera', '~> 1.0'
   s.add_dependency 'beaker-hostgenerator', '~> 2.2'
-  s.add_dependency 'beaker_puppet_helpers', '~> 1.3'
+  s.add_dependency 'beaker_puppet_helpers', '~> 2.1'
   s.add_dependency 'beaker-rspec', '~> 8.0', '>= 8.0.1'
   s.add_dependency 'beaker-vagrant', '~> 1.2'
   s.add_dependency 'puppet-modulebuilder', '~> 2.0', '>= 2.0.2'

commit c1e4176e83f39603a62f9a8686c4f71130a8dfe0

Support none and preinstalled collections again
When OpenVox support was added it didn't take none and preinstalled.
beaker_puppet_helpers 2.2 introduced support for the none collection so
that logic is pushed into the helper.

This is a change where preinstalled now also requires the package itself
to be preinstalled, rather than just the repository.

Fixes: d89ef932b74a ("Implement OpenVox support")

lib/voxpupuli/acceptance/spec_helper_acceptance.rb
@@ -18,13 +18,16 @@ def configure_beaker(modules: :metadata, &block)
 
   unless ENV['BEAKER_PROVISION'] == 'no'
     block_on hosts, run_in_parallel: true do |host|
-      unless %w[none preinstalled].include?(collection)
-        BeakerPuppetHelpers::InstallUtils.install_puppet_release_repo_on(host, collection)
+      if collection != 'preinstalled'
+        if collection != 'none'
+          BeakerPuppetHelpers::InstallUtils.install_puppet_release_repo_on(host, collection)
+        end
+
+        package_name = ENV.fetch('BEAKER_PUPPET_PACKAGE_NAME') do
+          BeakerPuppetHelpers::InstallUtils.collection2packagename(host, collection)
+        end
+        host.install_package(package_name)
       end
-      package_name = ENV.fetch('BEAKER_PUPPET_PACKAGE_NAME') do
-        BeakerPuppetHelpers::InstallUtils.collection2packagename(host, collection, prefer_aio: collection != 'none')
-      end
-      host.install_package(package_name)
 
       # by default, puppet-agent creates /etc/profile.d/puppet-agent.sh which adds /opt/puppetlabs/bin to PATH
       # in our non-interactive ssh sessions we manipulate PATH in ~/.ssh/environment, we need to do this step here as well

voxpupuli-acceptance.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'beaker-docker', '~> 2.1'
   s.add_dependency 'beaker-hiera', '~> 1.0'
   s.add_dependency 'beaker-hostgenerator', '~> 2.2'
-  s.add_dependency 'beaker_puppet_helpers', '~> 2.1'
+  s.add_dependency 'beaker_puppet_helpers', '~> 2.2'
   s.add_dependency 'beaker-rspec', '~> 8.0', '>= 8.0.1'
   s.add_dependency 'beaker-vagrant', '~> 1.2'
   s.add_dependency 'puppet-modulebuilder', '~> 2.0', '>= 2.0.2'