3ded1f77d53f7c4f621886abb46b28f8094e38b0
Tim Meusel <tim@bastelfreak.de>
2025-05-27 11:37:07 +0200

    Add new shared example with debug output
    This is the same as our existing shared example, but it runs `puppet apply --debug...`

--- lib/voxpupuli/acceptance/examples.rb
+++ lib/voxpupuli/acceptance/examples.rb
@@ -12,6 +12,18 @@ shared_examples 'an idempotent resource' do |host|
   end
 end
 
+shared_examples 'an idempotent resource with debug' do |host|
+  host ||= default
+
+  it 'applies with no errors' do
+    apply_manifest_on(host, manifest, catch_failures: true, debug: true)
+  end
+
+  it 'applies a second time without changes' do
+    apply_manifest_on(host, manifest, catch_changes: true, debug: true)
+  end
+end
+
 shared_examples 'the example' do |name, host|
   include_examples 'an idempotent resource', host do
     let(:manifest) do
